f579221091600a87d748546d0d3f3e3c49d07e48
akash27nik <114089138+akash27nik@users.noreply.github.com>
2025-12-29 20:24:56 +0530

    Add new CORS origin for socket server

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-frontend-sx6b.onrender.com",
+    origin: ["https://chat-app-frontend-sx6b.onrender.com", "https://chit-chatfriendly.vercel.app"],
     credentials: true,
   },
 });
